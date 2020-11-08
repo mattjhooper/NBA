1da4a7aee1fe6201fed795f1caa752e66a03a877
Matthew Hooper <mhooper@scottlogic.com>
2020-11-08 17:43:59 +0000

    Get Initial project setup

---
+++ NBA.Lib/NBA.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NBA.Lib
+{
+    public class Nba
+    {
+        public static string NbaCup(string resultSheet, string toFind) 
+        {
+            return "Boston Celtics:W=4;D=0;L=0;Scored=403;Conceded=350;Points=12";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# NBA" 
+# NBA
+
+https://www.codewars.com/kata/5a420163b6cfd7cde5000077/train/csharp
